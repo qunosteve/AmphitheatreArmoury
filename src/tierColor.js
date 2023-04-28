416522dc8b1fe8a4828e8b4914b63105b2a975e5
qunosteve <stephencano56@hotmail.com>
2023-04-28 01:34:44 -0500

    Added common option

--- src/tierColor.js
+++ src/tierColor.js
@@ -1,13 +1,13 @@
 // Define the color values for each option
 const colorOptions = {
+    common: 'ivory',
     honoured: '#ead5c2',
     exalted: '#7ed321',
     majestic: '#48baff',
     supreme: '#b86cf8',
     heroic: '#d40000',
     legendary: '#f58223',
-    mythic: '#ff00ff',
-    none: 'ivory'
+    mythic: '#ff00ff'
   };
     
   // Function to change the text color based on the input value
