c32a109e84dcae24d01400117eb0a51c029d785f
qunosteve <stephencano56@hotmail.com>
2023-04-29 15:50:28 -0500

    revered js

--- src/tokenStringOperations.js
+++
@@ -1,9 +0,0 @@
-function shortenTokenLength(tokenName) {
-    const tokenWords = tokenName.split(" ");
-    let formattedItemName = "";
-    for (let i = 4; i < tokenWords.length; i++) {
-      formattedItemName +=
-        tokenWords[i] + (i + 1 < tokenWords.length ? " " : "");
-    }
-    return formattedItemName;
-  }
